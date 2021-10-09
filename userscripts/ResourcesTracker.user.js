96ff68b3bc32848d671e81f319c46d8a85bced78
Dael <daelidle@gmail.com>
2021-10-09 13:48:16 +0200

    Fixed omitted resources on Resource Tracker

--- userscripts/ResourcesTracker.user.js
+++ userscripts/ResourcesTracker.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Idlescape Resources Tracker
 // @namespace    DaelIS
-// @version      1.1.1
+// @version      1.1.2
 // @description  Show a summary for all resources gained/lost since you logged in
 // @author       Dael
 // @updateURL    https://raw.githubusercontent.com/daelidle/ISscripts/main/userscripts/ResourcesTracker.user.js
 // @downloadURL  https://raw.githubusercontent.com/daelidle/ISscripts/main/userscripts/ResourcesTracker.user.js
 // @require      https://raw.githubusercontent.com/HighOnMikey/idlescape-socketio-listener/main/src/idlescape-listener.js
+// @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/ResourcesTracker/ResourceTracker.js
+// @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/ResourcesTracker/TrackerUI.js
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/utils/GeneralUtils.js
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/utils/ItemUtils.js
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/data/IdlescapeGameData.js
