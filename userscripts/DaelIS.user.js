f179c1ad65bfffcb35de2f127d03c892fb51a346
Dael <daelidle@gmail.com>
2023-03-21 21:10:05 +0100

    New game subdomain

--- userscripts/DaelIS.user.js
+++ userscripts/DaelIS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Idlescape - DaelIS Userscripts
 // @namespace    DaelIS
-// @version      2.0.11
+// @version      2.0.12
 // @description  Collection of utilities and QOL improvements for Idlescape.
 // @author       Dael
 // @icon         https://raw.githubusercontent.com/daelidle/ISscripts/main/assets/images/config-icon.png
@@ -50,8 +50,7 @@
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/utils/ItemUtils.js
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/src/utils/UiUtils.js
 // @require      https://raw.githubusercontent.com/daelidle/ISscripts/main/data/IdlescapeGameData.js
-// @match        https://www.idlescape.com/*
-// @match        https://idlescape.com/*
+// @match        https://play.idlescape.com/*
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
